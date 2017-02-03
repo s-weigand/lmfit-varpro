e3dbc9f6d3a81a40bf5e19f39e53fadcf4c80c92
Joern Weissenborn <joern.weissenborn@gmail.com>
2017-02-03 15:56:16 +0100

    initial commit :sunrise:

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="lmfit-varpro",
+    version="0.1.0",
+    description='A variable projection implementation for Python/lmfit.',
+    url='http://github.com/ThingiverseIO/pythingiverseio',
+    author='Joris Snellenburg, Stefan Schuetz, Joern Weissenborn',
+    author_email="""j.snellenburg@vu.nl,
+                    YamiNoKeshin@gmail.com,
+                    joern.weissenborn@gmail.com""",
+    license='GPLv3',
+    packages=['lmfit-varpro'],
+    install_requires=[
+        'numpy',
+        'lmfit',
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
