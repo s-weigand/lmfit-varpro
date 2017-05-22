455ebffe6a046f1a6de153c384dda4434ce06f06
Joern Weissenborn <joern.weissenborn@gmail.com>
2017-05-22 15:46:56 +0200

    changed e matrix from group to single matrix. resolves #4

--- lmfit_varpro/separable_model.py
+++ lmfit_varpro/separable_model.py
@@ -18,7 +18,7 @@ class SeparableModel(object):
         raise NotImplementedError
 
     def eval(self, parameter, *args, **kwargs):
-        e = self.e_matrix(parameter, **kwargs)
+        e = self.e_matrix(parameter, *args, **kwargs)
         c = self.c_matrix(parameter, *args, **kwargs)
         noise = kwargs["noise"] if "noise" in kwargs else False
         res = dot(e, c)
@@ -45,15 +45,9 @@ class SeparableModel(object):
 
     def retrieve_e_matrix(self, parameter, data, *args, **kwargs):
         c_matrix = self.c_matrix(parameter, *args, **kwargs)
-        #  e_matrix = []
-
-        # We expect that the c matrix is grouped along the 0 dim of data,
-        # whereas the e_matrix is grouped along dim 1
-        # TODO: we only calculate 1 e matrix, instead one for each group.
 
         dim0 = len(c_matrix)
         dim1 = c_matrix[0].shape[1]
-        dim2 = c_matrix[0].shape[0]
 
         e = np.empty((dim0, dim1), np.float64)
         for i in range(data.shape[0]):
@@ -61,4 +55,4 @@ class SeparableModel(object):
             d = data[i, :]
             e[i, :] = qr_coefficents(c, d)[:dim1]
 
-        return [e for _ in range(dim2)]
+        return e
--- lmfit_varpro/util.py
+++ lmfit_varpro/util.py
@@ -2,15 +2,11 @@ import numpy as np
 
 
 def dot(e, c):
-    res = np.empty((len(c), len(e)), dtype=np.float64)
+    dim1 = e.shape[0]
+    dim2 = c[0].shape[0]
+    res = np.empty((dim1, dim2), dtype=np.float64)
 
-    for x, t in iter_c_and_e(c, e):
-        res[x, t] = np.dot(c[x][t, :], e[t][x, :].T)
+    for i in range(len(c)):
+        res[i, :] = np.dot(c[i], e[i, :])
 
     return res
-
-
-def iter_c_and_e(c, e):
-    for i in range(len(c)):
-        for j in range(len(e)):
-            yield i, j
