5243ff14788ded4385e42e47213569c8de0aa156
Joris Snellenburg <jsnel@users.noreply.github.com>
2018-07-22 03:17:08 +0200

    Updated setup.py for 0.0.1 release to PyPI
    Disclaimer: This release is not intended for public usage, API is NOT stable.

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="lmfit-varpro",
-    version="0.1.0",
+    version="0.0.1",
     description='A variable projection implementation for Python/lmfit.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/glotaran/lmfit-varpro',
+    download_url = 'https://github.com/glotaran/lmfit-varpro/archive/0.0.1.tar.gz',
+    keywords = ['alpha'],
     author='Joris Snellenburg, '
            'Joern Weissenborn',
-    author_email="""j.snellenburg@vu.nl,
+    author_email="""j.snellenburg@gmail.com,
                     joern.weissenborn@gmail.com""",
     license='GPLv3',
-    packages=['lmfit_varpro'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+	],
     install_requires=[
         'numpy',
         'lmfit',
