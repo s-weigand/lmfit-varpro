434fdc718c94d9188a3adc934d15a72557681279
Sebastian Weigand <s.weigand.phy@gmail.com>
2018-07-25 14:56:53 +0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lmfit-varpro",
-    version="0.0.1",
+    version="version='0.0.2'",
     description='A variable projection implementation for Python/lmfit.',
     long_description=long_description,
     long_description_content_type="text/markdown",
