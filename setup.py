b161aadb58ad54b03055bcf3fa475309ce602ae5
Sebastian Weigand <s.weigand.phy@gmail.com>
2018-07-25 15:53:54 +0200

    changed quotes ... again

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lmfit-varpro",
-    version="version='0.0.11'",
+    version='0.0.11',
     description='A variable projection implementation for Python/lmfit.',
     long_description=long_description,
     long_description_content_type="text/markdown",
